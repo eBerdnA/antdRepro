adbe8c196b2828956bd319c6eb67e7fd3ba5011f
André Bering <andre@bering.in>
2020-07-22 15:40:36 +0200

    repro for antd

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Tree } from 'antd';
+
+ReactDOM.render(
+	<Tree />,
+	document.getElementById("content")
+);
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+// npx webpack --watch
+// watch issue: https://github.com/webpack/webpack/issues/2949
+
+module.exports = {
+	mode: "production",
+
+	devtool: "source-map",
+
+	watchOptions: {
+        poll: true
+    },
+	resolve: {
+		extensions: [".ts", ".tsx"]
+	},
+	output: {
+		path: path.resolve(__dirname, "wwwroot/dist"),
+		filename: "[name].js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts(x?)$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: "ts-loader"
+					}
+				]
+			},
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader"
+			}
+		]
+	},
+
+	externals: {
+		"react": "React",
+		"react-dom": "ReactDOM",
+		"react-router-dom": "ReactRouterDOM"
+	}
+};
